65f4378dbd65d357f4ccd596cdbb487ee6fcc2cd
ttedgar <150164985+ttedgar@users.noreply.github.com>
2024-09-26 19:46:29 +0200

    create configuration for source and target persistence layer

--- src/main/java/com/codecool/codecooljobhomework/CodecoolJobHomeworkApplication.java
+++ src/main/java/com/codecool/codecooljobhomework/CodecoolJobHomeworkApplication.java
@@ -2,8 +2,10 @@ package com.codecool.codecooljobhomework;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class CodecoolJobHomeworkApplication {
 
     public static void main(String[] args) {
